c8895c2639f4151a3a91586b03c957c20e0661a6
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-06-25 12:35:41 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -0,0 +1,16 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Jonathan Alcontin on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+{
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+@end
