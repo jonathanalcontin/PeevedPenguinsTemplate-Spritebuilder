89fdcc117328ddbd12a0da86c1fd487e4cf8c403
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-06-26 10:26:29 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -32,10 +32,10 @@
 }
 
 // called on every touch in this scene
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    [self launchPenguin];
-    
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
     CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
     // start catapult dragging when a touch inside of the catapult arm occurs
     if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
     {
@@ -45,7 +45,6 @@
         // setup a spring joint between the mouseJointNode and the catapultArm
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
     }
-
 }
 
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
