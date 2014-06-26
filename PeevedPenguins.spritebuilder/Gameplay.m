004fe437b55fcd28c2b1cd31dcb6e5e769cc47b7
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-06-26 10:50:30 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -132,10 +132,6 @@
     
 }
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
-{
-    CCLOG(@"Something collided with a seal!");
-}
 
 
 @end
