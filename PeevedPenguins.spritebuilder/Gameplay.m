2312b9de2d0a5de40ac5542d82ff8d329d1992a2
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-07-03 09:56:58 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -31,7 +31,7 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
-    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    CCScene *level = [CCBReader loadAsScene:@"Level1"];
     [_levelNode addChild:level];
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
